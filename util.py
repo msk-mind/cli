f56877446ff0d5221ea31bcd78a532d8a5b455a3
doori <doori.rose@gmail.com>
2020-04-30 16:29:49 -0400

    remove payload when error

--- util.py
+++ util.py
@@ -7,10 +7,11 @@ def print_mind_reponse(res):
     :return: pretty print
     """
     # if successful, show only status and payload
+    res_dict = res.to_dict()
     if res.status == "OK":
-        res_dict = res.to_dict()
         res_dict.pop("summary")
         res_dict.pop("description")
         print(pprint.pformat(res_dict))
     else:
-        print(res)
+        res_dict.pop("payload")
+        print(pprint.pformat(res_dict))
